523c1fedff830b92a69b1dc192b3d880f7f1ac92
松下 大輝(Daiki Matsushita) <77758521+Daiki-Matsushita@users.noreply.github.com>
2023-10-14 16:21:11 +0900

    Initial commit

---
+++ src/components/StyledJsx.jsx
@@ -0,0 +1,20 @@
+export const StyledJsx = () => {
+  return (
+    <>
+      <div className="container">
+        <p>- Styled JSX -</p>
+        <button>FIGHT!!</button>
+      </div>
+      <style jsx="true">{`
+        .container {
+          border: solid 2px #392eff;
+          border-radius: 20px;
+          padding: 8px;
+          display: flex;
+          justify-content: space-araund;
+          align-items: center;
+        }
+      `}</style>
+    </>
+  );
+};
